f4473f759df5e17e83ee8f8eb588bc7288e5a430
Ilakiya N <ilakzilakzilakz@gmail.com>
2025-07-10 09:47:29 +0000

    Initial Hangout Cafe website

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import Header from './components/Header';
+import Menu from './components/Menu';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <Header />
+      <Menu />
+      <Contact />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
