bc2d9302dde3c38781a5abf37dd40531509916b2
Ilakiya N <ilakzilakzilakz@gmail.com>
2025-07-11 07:09:28 +0000

    Updated the add to card section in the repo

---
+++ src/data/cakes.ts
@@ -0,0 +1,7 @@
+const cakes = [
+  { name: 'Black Forest Cake', image: '/cakes/cake1.png', priceHalf: 350, priceOne: 650 },
+  { name: 'White Forest Cake', image: '/cakes/cake2.png', priceHalf: 350, priceOne: 650 },
+  { name: 'Red velvet Cake', image: '/cakes/cake3.png', priceHalf: 400, priceOne: 850 },
+  { name: 'Choco Tuffle Cake', image: '/cakes/cake4.png', priceHalf: 450, priceOne: 1000 },
+];
+export default cakes;
