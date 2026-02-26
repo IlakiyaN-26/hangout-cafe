23b40be19d53b7620ac97cca71e0d4ec8e9984bc
Ilakiya N <ilakzilakzilakz@gmail.com>
2026-02-25 21:58:08 -0800

    Update contact information in Contact component

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,9 +4,9 @@ const Contact: React.FC = () => {
   return (
     <section>
       <h2>Contact Us ☕</h2>
-      <p>📞 Phone: +91 6381816307</p>
-      <p>📧 Email: hangoutcafe@example.com</p>
-      <p>📍 Address: Thaliyamagathevar theru , Vadasery, Nagercoil</p>
+      <p>📞 Phone: +91 123456789</p>
+      <p>📧 Email: cafe@example.com</p>
+      <p>📍 Address: Vadasery, Nagercoil</p>
     </section>
   );
 };
